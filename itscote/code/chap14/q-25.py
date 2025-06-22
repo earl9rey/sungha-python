7b8aff9ff08f24aff4e4b2e14c8aa16a9b463f2b
earl9rey <earl9rey@naver.com>
2025-06-22 23:33:08 +0900

    feat: Chap14 정렬/Q-25/1차 풀이

---
+++ itscote/code/chap14/q-25.py
@@ -0,0 +1,23 @@
+# 정렬 문제
+# p.361 Q-25 실패율 (시간 초과)
+
+def solution(N, stages):
+    result = []
+    answer = []
+    for i in range(N) :
+        n = sum(1 for x in stages if x == i+1) # if 문의 조건에 맞는 배열 원소의 개수 찾기 (1씩 더해가며)
+        d = sum(1 for x in stages if x >= i+1)
+        
+        if d == 0 :
+            fail = 0
+        else:
+            fail = n / d
+        
+        result.append([i+1, fail])
+        
+    result.sort(key=lambda x: -x[1])      
+    
+    for j in result:
+        answer.append(j[0])
+        
+    return answer

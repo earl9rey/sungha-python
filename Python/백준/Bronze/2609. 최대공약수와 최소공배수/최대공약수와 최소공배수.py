19264a4e81b75c02da68673440a4ca3d058c28d4
earl9rey <129039817+earl9rey@users.noreply.github.com>
2025-08-11 12:41:24 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ Python/백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+
+M = max(a, b)
+m = min(a, b)
+
+for i in range(m, 0, -1) :
+    if a % i == 0 and b % i == 0 :
+        gcd = i
+        break
+
+lcm = gcd * (a // gcd) * (b // gcd)
+
+print(gcd)
+print(lcm)
+
+

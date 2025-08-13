29c355cf8571e91b55a09555a59a01c0d9f8132f
earl9rey <129039817+earl9rey@users.noreply.github.com>
2025-08-13 12:24:10 +0900

    [Silver V] Title: 단어 정렬, Time: 560 ms, Memory: 37024 KB -BaekjoonHub

---
+++ Python/백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,22 @@
+# 알파벳 소문자로 이루어진 N개의 단어가 들어오면 아래와 같은 조건에 따라 정렬하는 프로그램을 작성하시오.
+
+# 1. 길이가 짧은 것부터
+# 2. 길이가 같으면 사전 순으로
+
+# 단, 중복된 단어는 하나만 남기고 제거해야 한다.
+
+n = int(input())
+word = []
+for _ in range(n) :
+    w = input()
+    word.append(w)
+
+# 중복 제거
+word = list(set(word))
+
+# 정렬: (길이, 사전순)
+word.sort(key=lambda x: (len(x), x))
+
+# 출력
+for w in word:
+    print(w)

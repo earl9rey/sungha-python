450c28897def08cf94f9a0dbe0c13a1f7020b84e
earl9rey <129039817+earl9rey@users.noreply.github.com>
2026-06-02 15:16:09 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 1.04 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.py
@@ -0,0 +1,33 @@
+# def solution(s):
+#     count_one = s.count("1")
+#     one = int("1" * count_one)  
+#     zero = len(s) - count_one
+
+#     result = []
+#     count = 0
+
+#     while True:
+#         if one == 1:
+#             break
+
+#         result.append(one % 2)
+#         one = one // 2
+#         count += 1
+
+#     answer = [count, zero]
+
+#     return answer
+
+def solution(s):
+    count = 0
+    zero = 0
+
+    while s != "1":
+        zero += s.count("0")
+
+        length = len(s) - s.count("0")
+        s = bin(length)[2:]
+
+        count += 1
+
+    return [count, zero]

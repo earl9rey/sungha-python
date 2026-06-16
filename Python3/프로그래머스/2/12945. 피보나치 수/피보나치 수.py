fba9e51c93adbf1ce1686d7c952b6c717fe42452
earl9rey <129039817+earl9rey@users.noreply.github.com>
2026-06-16 16:07:12 +0900

    [level 2] Title: 피보나치 수, Time: 383.62 ms, Memory: 458 MB -BaekjoonHub

---
+++ Python3/프로그래머스/2/12945. 피보나치 수/피보나치 수.py
@@ -0,0 +1,11 @@
+def solution(n):
+    lst = [0] * (n+1)
+    lst[0] = 0
+    lst[1] = 1
+    
+    for i in range(2, n+1):
+        lst[i] = lst[i-2] +lst[i-1]
+        
+    answer = lst[n] % 1234567
+    
+    return answer

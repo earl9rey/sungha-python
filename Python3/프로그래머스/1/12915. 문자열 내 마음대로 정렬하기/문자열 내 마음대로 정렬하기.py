f3e964dbfb2f73193aca82ddf4a9f260d22211ea
earl9rey <129039817+earl9rey@users.noreply.github.com>
2026-07-02 14:30:42 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.01 ms, Memory: 11.2 MB -BaekjoonHub

--- Python3/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
+++ Python3/프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.py
@@ -1,11 +1,15 @@
+# def solution(strings, n):
+#     strings.sort()
+    
+#     arr = []
+#     for s in strings:
+#         arr.append([s[n], s])
+    
+#     arr.sort(key=lambda x: x[0])
+#     answer = [x[1] for x in arr]
+    
+#     return answer
+
 def solution(strings, n):
-    strings.sort()
-    
-    arr = []
-    for s in strings:
-        arr.append([s[n], s])
-    
-    arr.sort(key=lambda x: x[0])
-    answer = [x[1] for x in arr]
-    
-    return answer
+    strings.sort(key=lambda x : (x[n], x))
+    return strings
